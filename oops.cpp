abf2c883fdbc4fe9a6b1c4cfcb7feef124e8cf59
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-04-21 11:32:21 +0530

    Add Teacher class with properties and methods in oops.cpp

--- oops.cpp
+++ oops.cpp
@@ -1,10 +1,34 @@
 // #include <bits/stdc++.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+
+class Teacher{
+  public:
+  //properties or attributs
+    string name;
+    string subject;
+    string dept;
+    double salary;
+  //member functions or methods
+    void changeDepat(string newDept){
+      dept=newDept;
+    }
+
+
+    
+};
   
 int main(){
-  cout << "hello";
+  Teacher t1;
+  t1.name="Kundan";
+  t1.salary=29000;
+  t1.dept="cse";
+  t1.subject="dbms";
+  cout<<t1.name<<endl;
+  cout<<t1.subject<<endl;
+
   return 0;
 }
